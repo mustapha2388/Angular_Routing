ffe6fbf05d493c5b5d414acaf32b0b05aaf4f960
ABDELOUAHAD <mustapha.abdelouahad@capgemini.com>
2019-10-08 15:04:40 +0200

    Integration of Routing into project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,17 +7,30 @@ import { AppareilComponent } from './components/appareil/appareil.component';
 import { FormsModule } from "@angular/forms";
 import { AppareilService } from './services/appareil.service';
 import { AppareilViewComponent } from './components/appareil-view/appareil-view.component';
+import { Routes, RouterModule } from '@angular/router';
+import { AuthComponent } from './components/auth/auth.component';
+
+
+const appRoutes: Routes = [
+  {path:"appareils", component:AppareilViewComponent},
+  {path: "", component: AppareilViewComponent},
+  {path:"auth", component:AuthComponent}
+];
+
 
 @NgModule({
   declarations: [
     AppComponent,
     AppareilComponent,
-    AppareilViewComponent
+    AppareilViewComponent,
+    AuthComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot(appRoutes)
+
   ],
   providers: [AppareilService],
   bootstrap: [AppComponent]
